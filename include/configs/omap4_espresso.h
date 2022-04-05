84a772fc1c0e4f37a6a2bd94ac6fa949df38a0c0
Mighty <mightymb17@gmail.com>
2022-04-05 16:21:33 +0530

    Add more espresso files

---
+++ include/configs/omap4_espresso.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration settings for the Samsung Espresso board.
+ * See ti_omap4_common.h for OMAP4 common part
+ */
+
+#ifndef __CONFIG_ESPRESSO_H
+#define __CONFIG_ESPRESSO_H
+
+#include <configs/ti_omap4_common.h>
+
+#endif /* __CONFIG_ESPRESSO_H */
